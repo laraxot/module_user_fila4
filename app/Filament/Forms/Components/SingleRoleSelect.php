550214e39b5c6451c0a15b0675eae5878dbdb75a
marco76tv <marco.sottana@gmail.com>
2025-09-24 08:45:50 +0200

    .

--- app/Filament/Forms/Components/SingleRoleSelect.php
+++ app/Filament/Forms/Components/SingleRoleSelect.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
+<<<<<<< HEAD
 namespace Modules\User\Filament\Forms\Components;
+=======
+namespace Modules\Notify\Filament\Forms\Components;
+>>>>>>> 1315781 (.)
 
 use Filament\Forms\Components\Select;
 use Modules\User\Models\Role;
