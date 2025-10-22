ba32ad3d366f84e4947e2d09cc55954de1f3f0ed
marco76tv <marco.sottana@gmail.com>
2025-10-22 17:35:28 +0200

    .

--- app/Models/Traits/IsProfileTrait.php
+++ app/Models/Traits/IsProfileTrait.php
@@ -271,7 +271,7 @@ trait IsProfileTrait
      * Get the user's user_name.
      * Ottiene il nome utente dal modello utente collegato.
      *
-     * @return Attribute<string|null, never>
+     * @return Attribute
      */
     protected function userName(): Attribute
     {
@@ -289,7 +289,7 @@ trait IsProfileTrait
      * Get the user's avatar URL.
      * Recupera l'URL dell'avatar dell'utente dalla MediaLibrary.
      *
-     * @return Attribute<string, never>
+     * @return Attribute
      */
     protected function avatar(): Attribute
     {
