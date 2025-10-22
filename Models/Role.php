feb6e1376a31b57bf4e0f0adcf3b574b9cfd9618
marco76tv <marco.sottana@gmail.com>
2025-10-22 09:10:03 +0200

    .

---
+++ Models/Role.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Role as SpatieRole;
+
+class Role extends SpatieRole
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'guard_name',
+        'display_name',
+        'description',
+    ];
+
+    public static function firstOrCreate(array $attributes, array $values = []): self
+    {
+        // @phpstan-ignore-next-line
+        return parent::firstOrCreate($attributes, $values);
+    }
+}
