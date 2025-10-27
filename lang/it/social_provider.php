ce5e0b87cd0a3300527875641bd8e6915d7279bb
marco76tv <marco.sottana@gmail.com>
2025-10-27 12:10:12 +0100

    .

--- lang/it/social_provider.php
+++ lang/it/social_provider.php
@@ -1,6 +1,9 @@
 <?php
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 6d20fbe (.)
 declare(strict_types=1);
 
 return [
@@ -59,6 +62,7 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
 =======
 return array (
   'resources' => 'Risorse',
@@ -127,3 +131,5 @@ return array (
   ),
 );
 >>>>>>> fbc8f8e (.)
+=======
+>>>>>>> 6d20fbe (.)
