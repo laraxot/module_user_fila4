47696c616cdbf4381d637bdf8925e36b5d766ead
marco76tv <marco.sottana@gmail.com>
2025-11-05 16:50:03 +0100

    .

--- app/Models/AuthenticationLog.php
+++ app/Models/AuthenticationLog.php
@@ -31,11 +31,7 @@ use Override;
  * @property Carbon|null $updated_at
  * @property string|null $updated_by
  * @property string|null $created_by
-<<<<<<< HEAD
- * @property Model $authenticatable
-=======
  * @property Model|\Eloquent $authenticatable
->>>>>>> ceff73a (.)
  * @property ProfileContract|null $creator
  * @property ProfileContract|null $updater
  *
@@ -59,10 +55,7 @@ use Override;
  * @method static Builder|AuthenticationLog whereUserAgent($value)
  *
  * @mixin IdeHelperAuthenticationLog
-<<<<<<< HEAD
-=======
  * @mixin \Eloquent
->>>>>>> ceff73a (.)
  */
 class AuthenticationLog extends BaseModel
 {
