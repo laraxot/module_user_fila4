5823428da3550fb603b84e2cfc8f1fece804b47b
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:44:25 +0100

    .

--- app/Filament/Widgets/RegistrationWidget.php
+++ app/Filament/Widgets/RegistrationWidget.php
@@ -16,11 +16,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Wizard\Step;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
-<<<<<<< HEAD
-use Filament\Schemas\Schema;
-=======
 use Filament\Forms\Form;
->>>>>>> 2805232 (.)
 use Filament\Widgets\Widget;
 use Illuminate\Auth\Events\Registered;
 >>>>>>> 3753a57 (.)
