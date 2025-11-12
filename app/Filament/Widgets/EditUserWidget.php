b4dd3dbc3372f46856121f5d76b250f3fe827d1c
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:45:08 +0100

    .

--- app/Filament/Widgets/EditUserWidget.php
+++ app/Filament/Widgets/EditUserWidget.php
@@ -24,7 +24,11 @@ use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Wizard\Step;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+<<<<<<< HEAD
 use Filament\Schemas\Schema;
+=======
+use Filament\Forms\Form;
+>>>>>>> 2805232 (.)
 use Filament\Widgets\Widget;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\RedirectResponse;
