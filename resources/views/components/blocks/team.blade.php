54afa0d37fe0091ef2eebb447b8d84081120838b
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:30:41 +0100

    .

--- resources/views/components/blocks/team.blade.php
+++ resources/views/components/blocks/team.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6d20fbe (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> fbc8f8e (.)
-=======
->>>>>>> 6d20fbe (.)
 @props(['title', 'description', 'members'])
 
 <div class="py-16 bg-base-100">
--- resources/views/filament/widgets/password-expired.blade.php
+++ resources/views/filament/widgets/password-expired.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6d20fbe (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> fbc8f8e (.)
-=======
->>>>>>> 6d20fbe (.)
 <x-filament-widgets::widget>
     <div class="text-center mb-4">
         <div class="flex justify-center">
@@ -24,27 +15,11 @@ declare(strict_types=1);
         <p class="mt-2 text-gray-600">{{ __('user::password_expired.sub_heading') }}</p>
     </div>
     
-<<<<<<< HEAD
-<<<<<<< HEAD
     <x-filament-schemas::form wire:submit="resetPassword">
-=======
-    <x-filament-panels::form wire:submit="resetPassword">
->>>>>>> fbc8f8e (.)
-=======
-    <x-filament-schemas::form wire:submit="resetPassword">
->>>>>>> 6d20fbe (.)
         {{ $this->form }}
         
         <x-filament::button type="submit" class="mt-4">
             @lang('user::password_expired.actions.reset_password.label') <x-filament::loading-indicator class="h-5 w-5" wire:loading wire:target="submit"/>
         </x-filament::button>
-<<<<<<< HEAD
-<<<<<<< HEAD
     </x-filament-schemas::form>
-=======
-    </x-filament-panels::form>
->>>>>>> fbc8f8e (.)
-=======
-    </x-filament-schemas::form>
->>>>>>> 6d20fbe (.)
 </x-filament-widgets::widget>
