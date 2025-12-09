5bd438e1b6bde2723993c34b7165b4a61f30517e
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:19:06 +0100

    .

--- app/Contracts/TeamInvitationContract.php
+++ app/Contracts/TeamInvitationContract.php
@@ -4,14 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\User\Contracts;
 
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Builder;
-=======
-<<<<<<< HEAD
-use Illuminate\Database\Eloquent\Builder;
-=======
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 
@@ -26,10 +19,6 @@ use Illuminate\Support\Carbon;
  * @property Carbon|null  $updated_at
  * @property TeamContract $team
  *
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 81efa49 (.)
  * @method static Builder|TeamInvitationContract newModelQuery()
  * @method static Builder|TeamInvitationContract newQuery()
  * @method static Builder|TeamInvitationContract query()
@@ -39,20 +28,6 @@ use Illuminate\Support\Carbon;
  * @method static Builder|TeamInvitationContract whereRole($value)
  * @method static Builder|TeamInvitationContract whereTeamId($value)
  * @method static Builder|TeamInvitationContract whereUpdatedAt($value)
-<<<<<<< HEAD
-=======
-=======
- * @method static \Illuminate\Database\Eloquent\Builder|TeamInvitationContract newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|TeamInvitationContract newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|TeamInvitationContract query()
- * @method static \Illuminate\Database\Eloquent\Builder|TeamInvitationContract whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|TeamInvitationContract whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|TeamInvitationContract whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|TeamInvitationContract whereRole($value)
- * @method static \Illuminate\Database\Eloquent\Builder|TeamInvitationContract whereTeamId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|TeamInvitationContract whereUpdatedAt($value)
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
  *
  * @phpstan-require-extends Model
  *
--- resources/views/components/ui/text-link.blade.php
+++ resources/views/components/ui/text-link.blade.php
@@ -1,44 +1,10 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 81efa49 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
 <x-ui.link
     {{ $attributes->except('wire:navigate') }}
     class="text-gray-500 underline cursor-pointer dark:text-gray-400 dark:hover:text-gray-300 hover:text-gray-800">
     {{ $slot }}
-<<<<<<< HEAD
 </x-ui.link>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</x-ui.link>
-=======
-</x-ui.link>
->>>>>>> a12f125f4a (.)
-=======
-</x-ui.link>
->>>>>>> b93ef594b4 (.)
-=======
-</x-ui.link>
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
