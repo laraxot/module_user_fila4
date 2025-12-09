be293f050438e4f43d0f0bdd9ad79950110f538c
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:35:10 +0100

    .

--- app/Http/Livewire/Auth/Passwords/Reset.php
+++ app/Http/Livewire/Auth/Passwords/Reset.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\User\Http\Livewire\Auth\Passwords;
 
+<<<<<<< HEAD
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\PasswordBroker;
@@ -11,6 +12,17 @@ use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Model;
+=======
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Livewire\Features\SupportRedirects\Redirector;
+use Illuminate\Contracts\Auth\PasswordBroker;
+use Illuminate\Contracts\View\View;
+use Illuminate\Contracts\View\Factory;
+use Modules\Xot\Actions\File\ViewCopyAction;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Auth\Events\PasswordReset;
+>>>>>>> 2805232 (.)
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -18,8 +30,11 @@ use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rules\Password as PasswordRule;
 use Livewire\Component;
+<<<<<<< HEAD
 use Livewire\Features\SupportRedirects\Redirector;
 use Modules\Xot\Actions\File\ViewCopyAction;
+=======
+>>>>>>> 2805232 (.)
 use Webmozart\Assert\Assert;
 
 class Reset extends Component
@@ -71,16 +86,26 @@ class Reset extends Component
         );
 
         /* @phpstan-ignore argument.type */
+<<<<<<< HEAD
         Assert::string($response_lang = trans((string) $response));
 
         if ($response === Password::PASSWORD_RESET) {
             session()->flash($response_lang);
 
+=======
+        Assert::string($response_lang = trans($response));
+
+        if ($response === Password::PASSWORD_RESET) {
+            session()->flash($response_lang);
+>>>>>>> 2805232 (.)
             return redirect(route('home'));
         }
 
         $this->addError('email', $response_lang);
+<<<<<<< HEAD
 
+=======
+>>>>>>> 2805232 (.)
         return null;
     }
 
