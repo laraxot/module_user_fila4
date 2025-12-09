7bb1f9c875bb2db1ff35c53e4bf83f50df07a1e1
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:27:55 +0100

    .

--- app/Filament/Resources/UserResource/Pages/BaseCreateUser.php
+++ app/Filament/Resources/UserResource/Pages/BaseCreateUser.php
@@ -7,6 +7,20 @@ namespace Modules\User\Filament\Resources\UserResource\Pages;
 use Modules\User\Filament\Resources\UserResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord;
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+
+>>>>>>> origin/develop
+>>>>>>> 81efa49 (.)
 abstract class BaseCreateUser extends XotBaseCreateRecord
 {
     // //
