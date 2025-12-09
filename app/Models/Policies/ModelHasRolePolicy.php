8537ab7e5317d01cdc11d6d2983612f9b640cfe2
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:30:38 +0100

    .

--- app/Models/Policies/ModelHasRolePolicy.php
+++ app/Models/Policies/ModelHasRolePolicy.php
@@ -5,12 +5,17 @@ declare(strict_types=1);
 namespace Modules\User\Models\Policies;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Modules\User\Models\ModelHasRole;
 use Modules\Xot\Contracts\UserContract;
 =======
 use Modules\User\Contracts\UserContract;
 use Modules\User\Models\ModelHasRole;
 >>>>>>> fbc8f8e (.)
+=======
+use Modules\User\Models\ModelHasRole;
+use Modules\Xot\Contracts\UserContract;
+>>>>>>> 6d20fbe (.)
 
 class ModelHasRolePolicy extends UserBasePolicy
 {
@@ -26,6 +31,7 @@ class ModelHasRolePolicy extends UserBasePolicy
      * Determine whether the user can view the model.
      */
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function view(UserContract $user, ModelHasRole $_modelHasRole): bool
     {
         return $user->hasPermissionTo('model-has-role.view') || $user->hasRole('super-admin');
@@ -34,6 +40,11 @@ class ModelHasRolePolicy extends UserBasePolicy
     {
         return $user->hasRole('super-admin');
 >>>>>>> fbc8f8e (.)
+=======
+    public function view(UserContract $user, ModelHasRole $_modelHasRole): bool
+    {
+        return $user->hasPermissionTo('model-has-role.view') || $user->hasRole('super-admin');
+>>>>>>> 6d20fbe (.)
     }
 
     /**
@@ -48,6 +59,7 @@ class ModelHasRolePolicy extends UserBasePolicy
      * Determine whether the user can update the model.
      */
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function update(UserContract $user, ModelHasRole $_modelHasRole): bool
     {
         return $user->hasPermissionTo('model-has-role.update') || $user->hasRole('super-admin');
@@ -56,11 +68,17 @@ class ModelHasRolePolicy extends UserBasePolicy
     {
         return $user->hasRole('super-admin');
 >>>>>>> fbc8f8e (.)
+=======
+    public function update(UserContract $user, ModelHasRole $_modelHasRole): bool
+    {
+        return $user->hasPermissionTo('model-has-role.update') || $user->hasRole('super-admin');
+>>>>>>> 6d20fbe (.)
     }
 
     /**
      * Determine whether the user can delete the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function delete(UserContract $user, ModelHasRole $_modelHasRole): bool
     {
@@ -70,11 +88,17 @@ class ModelHasRolePolicy extends UserBasePolicy
     {
         return $user->hasRole('super-admin');
 >>>>>>> fbc8f8e (.)
+=======
+    public function delete(UserContract $user, ModelHasRole $_modelHasRole): bool
+    {
+        return $user->hasPermissionTo('model-has-role.delete') || $user->hasRole('super-admin');
+>>>>>>> 6d20fbe (.)
     }
 
     /**
      * Determine whether the user can restore the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function restore(UserContract $user, ModelHasRole $_modelHasRole): bool
     {
@@ -84,6 +108,11 @@ class ModelHasRolePolicy extends UserBasePolicy
     {
         return $user->hasRole('super-admin');
 >>>>>>> fbc8f8e (.)
+=======
+    public function restore(UserContract $user, ModelHasRole $_modelHasRole): bool
+    {
+        return $user->hasPermissionTo('model-has-role.restore') || $user->hasRole('super-admin');
+>>>>>>> 6d20fbe (.)
     }
 
     /**
@@ -92,9 +121,13 @@ class ModelHasRolePolicy extends UserBasePolicy
     public function forceDelete(UserContract $user, ModelHasRole $modelHasRole): bool
     {
 <<<<<<< HEAD
+<<<<<<< HEAD
         return $user->hasPermissionTo('model-has-role.force-delete') || $user->hasRole('super-admin');
 =======
         return $user->hasRole('super-admin');
 >>>>>>> fbc8f8e (.)
+=======
+        return $user->hasPermissionTo('model-has-role.force-delete') || $user->hasRole('super-admin');
+>>>>>>> 6d20fbe (.)
     }
 }
--- lang/it/password.php
+++ lang/it/password.php
@@ -1,6 +1,9 @@
 <?php
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 6d20fbe (.)
 declare(strict_types=1);
 
 return [
@@ -82,6 +85,7 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
 =======
 return array (
   'navigation' => 
@@ -187,3 +191,5 @@ return array (
   ),
 );
 >>>>>>> fbc8f8e (.)
+=======
+>>>>>>> 6d20fbe (.)
