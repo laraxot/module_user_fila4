b8ef5f9988b09e642cd96e07adddae2163f22758
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:29:29 +0100

    .

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -43,8 +43,6 @@ use Modules\Xot\Contracts\ProfileContract;
  * @mixin \Eloquent
  */
 /**
- * @property-read \Modules\Fixcity\Models\Profile|null $creator
- * @property-read \Modules\Fixcity\Models\Profile|null $updater
  * @method static \Modules\User\Database\Factories\FeatureFactory factory($count = null, $state = [])
  * @method static Builder<static>|Feature newModelQuery()
  * @method static Builder<static>|Feature newQuery()
