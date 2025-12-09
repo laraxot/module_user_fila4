bd47f5d205020bee9428cfa1778a39e784781429
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:34:17 +0100

    .

--- Database/Migrations/2025_05_16_221811_add_owner_id_to_teams_table.php
+++ Database/Migrations/2025_05_16_221811_add_owner_id_to_teams_table.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::connection('user')->table('teams', function (Blueprint $table): void {
-            if (!Schema::connection('user')->hasColumn('teams', 'owner_id')) {
+            if (! Schema::connection('user')->hasColumn('teams', 'owner_id')) {
                 $table->uuid('owner_id')->nullable()->after('id');
 
                 // opzionale: $table->foreign('owner_id')->references('id')->on('users')->nullOnDelete();
