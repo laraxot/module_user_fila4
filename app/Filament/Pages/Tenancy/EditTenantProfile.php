f7f58057dfbc921527c75371f8a6b14193fa14d0
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:12:52 +0100

    .

--- app/Filament/Pages/Tenancy/EditTenantProfile.php
+++ app/Filament/Pages/Tenancy/EditTenantProfile.php
@@ -9,15 +9,7 @@ use Filament\Schemas\Schema;
 use Filament\Schemas\Schema;
 use Modules\Xot\Datas\XotData;
 use Webmozart\Assert\Assert;
-<<<<<<< HEAD
-<<<<<<< HEAD
-use Filament\Forms\Form;
-=======
 use Filament\Schemas\Schema;
->>>>>>> 1724879 (.)
-=======
-use Filament\Schemas\Schema;
->>>>>>> cebb28c (.)
 
 class EditTenantProfile extends BaseEditTenantProfile
 {
@@ -26,11 +18,11 @@ class EditTenantProfile extends BaseEditTenantProfile
         return __('user::tenancy.navigation.edit');
     }
 
-    public function form(\Filament\Schemas\Schema $form): \Filament\Schemas\Schema
+    public function form(Schema $schema): Schema
     {
         $resource = XotData::make()->getTenantResourceClass();
 
-        Assert::isInstanceOf($res = $resource::form($form), Schema::class);
+        Assert::isInstanceOf($res = $resource::form($schema), Schema::class);
 
         return $res;
 
