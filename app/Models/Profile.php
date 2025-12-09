c268dcea132013343a034e21d3ccc86a3bdbc6ab
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:15:12 +0100

    .

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -89,11 +89,7 @@ class Profile extends BaseProfile implements HasMedia
      *
      * @var list<string>
      */
-<<<<<<< HEAD
     protected $formlessAttributes = [
-=======
-    protected $schemalessAttributes = [
->>>>>>> 4b219c8 (.)
         'extra',
     ];
 
