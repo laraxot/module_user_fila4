dcf353278bab0bb940bad4a9429947c4721f548d
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:39:15 +0100

    .

--- app/Models/SocialProvider.php
+++ app/Models/SocialProvider.php
@@ -76,7 +76,11 @@ class SocialProvider extends BaseModel
         // 'client_secret',// => env('FACEBOOK_CLIENT_SECRET'),
     ];
 
+<<<<<<< HEAD
     protected array $form = [
+=======
+    protected array $schema = [
+>>>>>>> 4b219c8 (.)
         'id' => 'integer',
         'name' => 'string',
         'scopes' => 'json',
