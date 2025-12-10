12be7dd60e14b671ef26c42a7be050f3ae416653
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:33:20 +0100

    .

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -33,7 +33,7 @@ class UserSeeder extends Seeder
         $this->command->info('👤 Inizializzazione seeding User...');
 
         // Disabilita i controlli di foreign key (solo per MySQL)
-        if (DB::getDriverName() !== 'sqlite') {
+        if ('sqlite' !== DB::getDriverName()) {
             DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         }
 
@@ -44,7 +44,7 @@ class UserSeeder extends Seeder
             $this->command->info('✅ Seeding User completato con successo!');
         } finally {
             // Riabilita i controlli di foreign key (solo per MySQL)
-            if (DB::getDriverName() !== 'sqlite') {
+            if ('sqlite' !== DB::getDriverName()) {
                 DB::statement('SET FOREIGN_KEY_CHECKS=1;');
             }
         }
