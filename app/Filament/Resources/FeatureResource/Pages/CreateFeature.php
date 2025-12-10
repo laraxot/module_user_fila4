aa81ddb6d3b289fba635c1649a47d7848b144e24
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:18:20 +0100

    .

--- app/Filament/Resources/FeatureResource/Pages/CreateFeature.php
+++ app/Filament/Resources/FeatureResource/Pages/CreateFeature.php
@@ -7,15 +7,7 @@ namespace Modules\User\Filament\Resources\FeatureResource\Pages;
 use Modules\User\Filament\Resources\FeatureResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord;
 
-<<<<<<< HEAD
 class CreateFeature extends XotBaseCreateRecord
-=======
-<<<<<<< HEAD
-class CreateFeature extends XotBaseCreateRecord
-=======
-class CreateFeature extends \Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
 {
     protected static string $resource = FeatureResource::class;
 }
--- lang/it/errors.php
+++ lang/it/errors.php
@@ -64,20 +64,4 @@ return [
             'description' => 'Il valore inserito è troppo corto.',
         ],
     ],
-<<<<<<< HEAD
 ];
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-];
-=======
-];
->>>>>>> a12f125f4a (.)
-=======
-];
->>>>>>> b93ef594b4 (.)
-=======
-];
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
