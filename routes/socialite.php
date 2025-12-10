64e0115fbe5021d274a1524b0c3d6a02b62ac114
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:26:18 +0100

    .

--- routes/socialite.php
+++ routes/socialite.php
@@ -10,14 +10,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::namespace('Socialite')
     ->name('socialite.')
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 81efa49 (.)
     ->group(static function (): void {
         Route::get(
             '/admin/login/{provider}',
@@ -26,32 +18,3 @@ Route::namespace('Socialite')
         )->name('oauth.redirect');
         Route::get('/sso/{provider}/callback', 'ProcessCallbackController')->name('oauth.callback');
     });
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-    ->group(
-        static function (): void {
-            Route::get(
-                '/admin/login/{provider}',
-                // 'LoginController@redirectToProvider',
-                'RedirectToProviderController',
-            )
-                ->name('oauth.redirect');
-            Route::get(
-                '/sso/{provider}/callback',
-                'ProcessCallbackController',
-            )
-                ->name('oauth.callback');
-        }
-<<<<<<< HEAD
-    );
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-    );
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
