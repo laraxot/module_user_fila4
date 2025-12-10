ed5e0fd93503dfffa55311e2107e65c3d8e65ca5
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:28:13 +0100

    .

--- app/Models/User.php
+++ app/Models/User.php
@@ -149,11 +149,7 @@ class User extends BaseUser
      *
      * @var string
      */
-<<<<<<< HEAD
     public $connection = 'user';
-=======
-    public $connection = 'mysql';
->>>>>>> 44e65d8 (.)
 
 <<<<<<< HEAD
     #[Override]
