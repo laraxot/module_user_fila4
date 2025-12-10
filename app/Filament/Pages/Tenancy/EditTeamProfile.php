c2e3407b4256d57ea0ef82880d963c3798317be9
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:20:46 +0100

    .

--- app/Filament/Pages/Tenancy/EditTeamProfile.php
+++ app/Filament/Pages/Tenancy/EditTeamProfile.php
@@ -17,7 +17,7 @@ class EditTeamProfile extends EditTenantProfile
     public function getFormSchema(): array
     {
         return [
-            TextInput::make('name'),
+            'name' => TextInput::make('name'),
             // ...
         ];
     }
