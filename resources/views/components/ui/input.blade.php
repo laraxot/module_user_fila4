1529f71696aa16829c1b3ce4c620664b575d585d
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:23:27 +0100

    .

--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 81efa49 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
 @props([
     'label' => null,
     'id' => null,
@@ -44,20 +26,4 @@ declare(strict_types=1);
     @error($wireModel)
         <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
     @enderror
-<<<<<<< HEAD
 </div>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</div>
-=======
-</div>
->>>>>>> a12f125f4a (.)
-=======
-</div>
->>>>>>> b93ef594b4 (.)
-=======
-</div>
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
--- resources/views/components/ui/marketing/header.blade.php
+++ resources/views/components/ui/marketing/header.blade.php
@@ -1,28 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 81efa49 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
 <header class="w-full bg-gradient-to-r from-blue-900 to-blue-800 shadow-md">
     <div class="relative z-20 flex items-center justify-between w-full h-20 max-w-7xl px-4 sm:px-6 lg:px-8 mx-auto">
         <!-- Logo e Menu Mobile -->
