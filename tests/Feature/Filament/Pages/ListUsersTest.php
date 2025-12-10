6b6db67287770f9e618b23cc896780f0d9057e4a
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:39:31 +0100

    .

--- tests/Feature/Filament/Pages/ListUsersTest.php
+++ tests/Feature/Filament/Pages/ListUsersTest.php
@@ -14,6 +14,7 @@ use Modules\User\Filament\Resources\UserResource\Pages\BaseListUsers;
 use Modules\User\Filament\Resources\UserResource\Pages\ListUsers;
 use Modules\User\Filament\Resources\UserResource\Widgets\UserOverview;
 use Modules\User\Models\User;
+use Tests\TestCase;
 
 uses(TestCase::class);
 
