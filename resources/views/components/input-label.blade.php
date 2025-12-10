a7329e9f92a9201976f7bb3c82c03525ff00f174
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:27:58 +0100

    .

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,27 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6d20fbe (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> fbc8f8e (.)
-=======
->>>>>>> 6d20fbe (.)
 @props(['value'])
 
 <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
     {{ $value ?? $slot }}
-<<<<<<< HEAD
-<<<<<<< HEAD
 </label>
-=======
-</label> 
->>>>>>> fbc8f8e (.)
-=======
-</label>
->>>>>>> 6d20fbe (.)
