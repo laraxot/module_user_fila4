3af0229177b9fecb2e8c40d1a552ef2da9975435
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:25:53 +0100

    .

--- app/Models/Policies/FeaturePolicy.php
+++ app/Models/Policies/FeaturePolicy.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace Modules\User\Models\Policies;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Modules\User\Models\Feature;
 use Modules\Xot\Contracts\UserContract;
 =======
 
 >>>>>>> fbc8f8e (.)
+=======
+use Modules\User\Models\Feature;
+use Modules\Xot\Contracts\UserContract;
+>>>>>>> 6d20fbe (.)
 
 class FeaturePolicy extends UserBasePolicy
 {
@@ -26,6 +31,7 @@ class FeaturePolicy extends UserBasePolicy
      * Determine whether the user can view the model.
      */
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function view(UserContract $user, Feature $_feature): bool
     {
         return $user->hasPermissionTo('feature.view') || $user->hasRole('super-admin');
@@ -35,6 +41,11 @@ class FeaturePolicy extends UserBasePolicy
 
                $user->hasRole('super-admin');
 >>>>>>> fbc8f8e (.)
+=======
+    public function view(UserContract $user, Feature $_feature): bool
+    {
+        return $user->hasPermissionTo('feature.view') || $user->hasRole('super-admin');
+>>>>>>> 6d20fbe (.)
     }
 
     /**
@@ -49,6 +60,7 @@ class FeaturePolicy extends UserBasePolicy
      * Determine whether the user can update the model.
      */
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function update(UserContract $user, Feature $_feature): bool
     {
         return $user->hasPermissionTo('feature.update') || $user->hasRole('super-admin');
@@ -58,11 +70,17 @@ class FeaturePolicy extends UserBasePolicy
 
                $user->hasRole('super-admin');
 >>>>>>> fbc8f8e (.)
+=======
+    public function update(UserContract $user, Feature $_feature): bool
+    {
+        return $user->hasPermissionTo('feature.update') || $user->hasRole('super-admin');
+>>>>>>> 6d20fbe (.)
     }
 
     /**
      * Determine whether the user can delete the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function delete(UserContract $user, Feature $_feature): bool
     {
@@ -73,11 +91,17 @@ class FeaturePolicy extends UserBasePolicy
 
                $user->hasRole('super-admin');
 >>>>>>> fbc8f8e (.)
+=======
+    public function delete(UserContract $user, Feature $_feature): bool
+    {
+        return $user->hasPermissionTo('feature.delete') || $user->hasRole('super-admin');
+>>>>>>> 6d20fbe (.)
     }
 
     /**
      * Determine whether the user can restore the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function restore(UserContract $user, Feature $_feature): bool
     {
@@ -88,6 +112,11 @@ class FeaturePolicy extends UserBasePolicy
 
                $user->hasRole('super-admin');
 >>>>>>> fbc8f8e (.)
+=======
+    public function restore(UserContract $user, Feature $_feature): bool
+    {
+        return $user->hasPermissionTo('feature.restore') || $user->hasRole('super-admin');
+>>>>>>> 6d20fbe (.)
     }
 
     /**
@@ -96,10 +125,14 @@ class FeaturePolicy extends UserBasePolicy
     public function forceDelete(UserContract $user, Feature $feature): bool
     {
 <<<<<<< HEAD
+<<<<<<< HEAD
         return $user->hasPermissionTo('feature.force-delete') || $user->hasRole('super-admin');
 =======
 
                $user->hasRole('super-admin');
 >>>>>>> fbc8f8e (.)
+=======
+        return $user->hasPermissionTo('feature.force-delete') || $user->hasRole('super-admin');
+>>>>>>> 6d20fbe (.)
     }
 }
