66dc2f44f0177d0a66dbd5b69f3e0f08927e2456
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:22:19 +0100

    .

--- database/migrations/2023_01_01_000004_create_team_user_table.php
+++ database/migrations/2023_01_01_000004_create_team_user_table.php
@@ -5,25 +5,7 @@ declare(strict_types=1);
 use Illuminate\Database\Schema\Blueprint;
 use Modules\Xot\Database\Migrations\XotBaseMigration;
 
-<<<<<<< HEAD
 return new class extends XotBaseMigration {
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-return new class extends XotBaseMigration {
-=======
-return new class extends XotBaseMigration
-{
->>>>>>> a12f125f4a (.)
-=======
-return new class extends XotBaseMigration {
->>>>>>> b93ef594b4 (.)
-=======
-return new class extends XotBaseMigration
-{
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
     /**
 <<<<<<< HEAD
      * Run the migrations.
@@ -39,14 +21,6 @@ return new class extends XotBaseMigration
     public function up(): void
     {
         // -- CREATE --
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 81efa49 (.)
         $this->tableCreate(static function (Blueprint $table): void {
 <<<<<<< HEAD
             // $table->uuid('id')->primary();
@@ -67,10 +41,6 @@ return new class extends XotBaseMigration
             $table->primary(['team_id', 'user_id']);
 >>>>>>> a382d4f1 (.)
         });
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 81efa49 (.)
 
         // -- UPDATE --
         $this->tableUpdate(function (Blueprint $table): void {
@@ -81,46 +51,5 @@ return new class extends XotBaseMigration
 
             // $this->updateUser($table);
         });
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> origin/develop
-        $this->tableCreate(
-            static function (Blueprint $table): void {
-                // $table->uuid('id')->primary();
-                $table->id();
-                $table->foreignId('team_id');
-                $table->uuid('user_id')->nullable()->index();
-                // $table->foreignIdFor(\Modules\Xot\Datas\XotData::make()->getUserClass());
-                $table->string('role')->nullable();
-                // $table->unique(['team_id', 'user_id']);
-            }
-        );
-
-        // -- UPDATE --
-        $this->tableUpdate(
-            function (Blueprint $table): void {
-                $this->updateTimestamps(table: $table, hasSoftDeletes: true);
-                // $this->updateUser($table);
-            }
-        );
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-
-        // -- UPDATE --
-        $this->tableUpdate(function (Blueprint $table): void {
-            $this->updateTimestamps(
-                table: $table,
-                hasSoftDeletes: true,
-            );
-
-            // $this->updateUser($table);
-        });
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
     }
 };
