1f956fe0c703f24f4193f104bae0e926cae08280
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:30:36 +0100

    .

--- app/Http/Volt/LogoutAction.php
+++ app/Http/Volt/LogoutAction.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\User\Http\Volt;
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 81efa49 (.)
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,6 +18,40 @@ use Illuminate\Support\Facades\Auth;
  *
  * #[Post('/logout', name: 'logout', middleware: ['web', 'auth'])]
  */
+<<<<<<< HEAD
+=======
+=======
+use Illuminate\Support\Facades\Auth;
+=======
+>>>>>>> b93ef594b4 (.)
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Volt\Routing\Attribute\Post;
+
+/*
+<<<<<<< HEAD
+=======
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use Volt\Routing\Attribute\Post;
+
+/*
+>>>>>>> origin/develop
+Attribute class Volt\Routing\Attribute\Post does not exist.
+
+#[Post('/logout', name: 'logout', middleware: ['web', 'auth'])]
+*/
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+ * Attribute class Volt\Routing\Attribute\Post does not exist.
+ *
+ * #[Post('/logout', name: 'logout', middleware: ['web', 'auth'])]
+ */
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 81efa49 (.)
 final class LogoutAction
 {
     public function __invoke(): RedirectResponse
