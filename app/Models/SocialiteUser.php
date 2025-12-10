953829a90a4bdfd64462a7f2ef28d2eb7a2c0037
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:19:15 +0100

    .

--- app/Models/SocialiteUser.php
+++ app/Models/SocialiteUser.php
@@ -64,6 +64,10 @@ use Modules\Xot\Datas\XotData;
  */
 class SocialiteUser extends BaseModel
 {
+<<<<<<< HEAD
+=======
+
+>>>>>>> fbc8f8e (.)
     /** @var list<string> */
     protected $fillable = [
         // 'id',
--- lang/pt_BR/timex.php
+++ lang/pt_BR/timex.php
@@ -7,15 +7,27 @@ return [
         'label' => 'Agendamento',
         'pluralLabel' => 'Agendamentos',
     ],
+<<<<<<< HEAD
+=======
+
+>>>>>>> fbc8f8e (.)
     'modal' => [
         'submit' => 'Criar',
         'cancel' => 'Cancelar',
         'delete' => 'Deletar',
         'edit' => 'Editar',
     ],
+<<<<<<< HEAD
     'events' => [
         'empty' => 'Nenhum evento futuro',
     ],
+=======
+
+    'events' => [
+        'empty' => 'Nenhum evento futuro',
+    ],
+
+>>>>>>> fbc8f8e (.)
     'event' => [
         'subject' => 'Título',
         'body' => 'Mensagem',
@@ -26,11 +38,19 @@ return [
         'participants' => 'Participantes',
         'attachments' => 'Anexos',
     ],
+<<<<<<< HEAD
+=======
+
+>>>>>>> fbc8f8e (.)
     'event-list' => [
         'author' => 'Author: :name',
         'start' => 'Start: :start',
         'end' => 'End: :end',
     ],
+<<<<<<< HEAD
+=======
+
+>>>>>>> fbc8f8e (.)
     'labels' => [
         'navigation' => 'TiMEX',
         'breadcrumbs' => 'TiMEX',
