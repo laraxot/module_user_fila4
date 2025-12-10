f3e401336e23d91b031610e8d15f21bc8087e77a
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:51:17 +0100

    .

--- app/Models/User.php
+++ app/Models/User.php
@@ -138,11 +138,7 @@ class User extends BaseUser
      *
      * @var string
      */
-<<<<<<< HEAD
-    public $connection = 'mysql';
-=======
     public $connection = 'user';
->>>>>>> ceff73a (.)
 
     #[\Override]
     public function canAccessSocialite(): bool
