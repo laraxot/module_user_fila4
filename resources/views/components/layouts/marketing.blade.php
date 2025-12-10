bd60745db49a3bce4038c78464f7eee8deb0d0a3
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:28:56 +0100

    .

--- resources/views/components/layouts/marketing.blade.php
+++ resources/views/components/layouts/marketing.blade.php
@@ -1,12 +1,46 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 81efa49 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 81efa49 (.)
 <x-layouts.main>
     
     <x-ui.marketing.header />
     
     {{ $slot }}
 
+<<<<<<< HEAD
 </x-layouts.main>
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+</x-layouts.main>
+=======
+</x-layouts.main>
+>>>>>>> a12f125f4a (.)
+=======
+</x-layouts.main>
+>>>>>>> b93ef594b4 (.)
+=======
+</x-layouts.main>
+>>>>>>> origin/develop
+>>>>>>> 81efa49 (.)
