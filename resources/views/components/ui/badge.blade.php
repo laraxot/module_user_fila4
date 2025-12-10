c93e8ac83b307c631655cc2162224a71a572ecda
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:30:21 +0100

    .

--- resources/views/components/ui/badge.blade.php
+++ resources/views/components/ui/badge.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 81efa49 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
 @props([
     'background' => 'bg-blue-600',
     'color' => 'text-white'
@@ -28,20 +10,4 @@ declare(strict_types=1);
 
 <span class="{{ $background }} {{ $color }} relative flex items-center text-xs font-semibold pl-2 pr-2.5 py-1 rounded-full">
     {{ $slot }}
-<<<<<<< HEAD
 </span>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</span>
-=======
-</span>
->>>>>>> a12f125f4a (.)
-=======
-</span>
->>>>>>> b93ef594b4 (.)
-=======
-</span>
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
--- resources/views/components/ui/marketing/breadcrumbs.blade.php
+++ resources/views/components/ui/marketing/breadcrumbs.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 81efa49 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
 @props([
     'crumbs' => [], 
     'page' => 'About'
@@ -53,20 +35,4 @@ Example Usage:
             @endif
         @endforeach
     </ol>
-<<<<<<< HEAD
 </nav>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</nav>
-=======
-</nav>
->>>>>>> a12f125f4a (.)
-=======
-</nav>
->>>>>>> b93ef594b4 (.)
-=======
-</nav>
->>>>>>> origin/develop
->>>>>>> 81efa49 (.)
