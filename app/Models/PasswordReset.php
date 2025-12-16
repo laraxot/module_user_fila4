9f69177d6120c6e62a803af3b6684d30235cd1af
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:38:22 +0100

    .

--- app/Models/PasswordReset.php
+++ app/Models/PasswordReset.php
@@ -50,8 +50,6 @@ use Modules\Xot\Contracts\ProfileContract;
  * @mixin \Eloquent
  */
 /**
- * @property-read \Modules\Fixcity\Models\Profile|null $creator
- * @property-read \Modules\Fixcity\Models\Profile|null $updater
  * @method static \Modules\User\Database\Factories\PasswordResetFactory factory($count = null, $state = [])
  * @method static Builder<static>|PasswordReset newModelQuery()
  * @method static Builder<static>|PasswordReset newQuery()
