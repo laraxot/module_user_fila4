f689ae07a3c3be333384536a1c26058bc664ab85
marco76tv <1944941+marco76tv@users.noreply.github.com>
2025-12-23 13:22:51 +0000

    Check & fix styling

--- database/migrations/2025_05_16_221811_create_teams_table.php
+++ database/migrations/2025_05_16_221811_create_teams_table.php
@@ -7,7 +7,6 @@
 declare(strict_types=1);
 
 use Illuminate\Database\Schema\Blueprint;
-use Modules\User\Models\Team;
 use Modules\Xot\Database\Migrations\XotBaseMigration;
 
 return new class extends XotBaseMigration {
