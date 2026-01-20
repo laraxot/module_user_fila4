4850b9cd272abefca9fcf0173558bfda577ca36b
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-01-20 06:32:06 +0000

    Check & fix styling

--- tests/Unit/Actions/GetCurrentDeviceActionTest.php
+++ tests/Unit/Actions/GetCurrentDeviceActionTest.php
@@ -2,17 +2,15 @@
 
 declare(strict_types=1);
 
-uses(\Modules\User\Tests\TestCase::class);
+uses(Modules\User\Tests\TestCase::class);
 
 use Modules\User\Actions\GetCurrentDeviceAction;
-use Modules\User\Models\Device;
-use Illuminate\Support\Facades\App;
 
 test('GetCurrentDeviceAction can be instantiated', function () {
     $action = new GetCurrentDeviceAction();
-    
+
     expect($action)->toBeInstanceOf(GetCurrentDeviceAction::class);
-    
+
     // Check that execute method exists using reflection
     expect(method_exists($action, 'execute'))->toBeTrue();
 });
